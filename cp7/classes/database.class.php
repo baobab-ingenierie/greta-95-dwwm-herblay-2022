ffb87394075e30b13fd9afa1bc516eac0179343f
lesly-lodin <lesly.lodin@baobab-ingenierie.fr>
2022-03-02 12:50:40 +0100

    CP7 Day 3

--- cp7/classes/database.class.php
+++ cp7/classes/database.class.php
@@ -19,7 +19,49 @@ class Database
     // Constantes de classe
     const PATTERN_HOST = '/^([A-Za-z]{1,30})$|^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$/';
     const PATTERN_OBJECT = '/^([A-Za-z_]{1,30})$/';
-    const PATTERN_PASSWORD = '/^([A-Za-z\d@$!%*?&]{,30})$/';
+    const PATTERN_PASSWORD = '/^([A-Za-z\d@$!%*?&_]{0,30})$/';
+
+    /**
+     * Constructeur de la classe
+     * @param string $newHost nom du serveur de BDD
+     * @param int $newPort port d'écoute du serveur de BDD
+     * @param string $newDbname nom de la BDD
+     * @param string $newUser nom de l'utilisateur
+     * @param string $newPass mot de passe
+     */
+
+    public function __construct(
+        string $newHost,
+        int $newPort,
+        string $newDbname,
+        string $newUser,
+        string $newPass
+    ) {
+        // Assigne la valeur des arguments aux attributs de la classe
+        $this->setHost($newHost);
+        $this->setPort($newPort);
+        $this->setDbname($newDbname);
+        $this->setUser($newUser);
+        $this->setPass($newPass);
+
+        // Tente une connexion à la BDD avec l'objet PDO
+        try {
+            // Se connecte à la BDD
+            $dsn = 'mysql:host=%s;dbname=%s;port=%d;charset=utf8';
+            $this->cnx = new PDO(
+                sprintf($dsn, $this->getHost(), $this->getDbname(), $this->getPort()),
+                $this->getUser(),
+                $this->getPass()
+            );
+
+            // Définit les attributs de connexion (gestion erreurs et renvoi données)
+            $this->cnx->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->cnx->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+            $this->isAuth = true;
+        } catch (PDOException $err) {
+            throw new PDOException(__CLASS__ . ' - ' . $err->getMessage());
+        }
+    }
 
     // Encapsulation : Accesseurs (ou getters)
     public function getHost(): string
@@ -47,12 +89,16 @@ class Database
         return $this->pass;
     }
 
+    public function getCnx(): PDO
+    {
+        return $this->cnx;
+    }
+
     // Encapsulation : mutateurs (ou setters)
 
     /**
      * Change la valeur de l'hôte (ou nom du serveur) de sorte qu'il
-     * corresponde aux modèles suivants :
-     * 127.0.0.1 ou localhost
+     * corresponde aux modèles suivants : 127.0.0.1 ou localhost (RegExp)
      */
 
     public function setHost(string $newHost)
@@ -61,7 +107,79 @@ class Database
         if (preg_match(self::PATTERN_HOST, $newHost) === 1) {
             $this->host = $newHost;
         } else {
-            throw new Exception(__CLASS__ . ' : Le nom de l\'hôte ne correspond pas au modèle demandé - ' . self::PATTERN_HOST);
+            throw new Exception(__CLASS__ . ' - Le nom de l\'hôte ne correspond pas au modèle demandé : ' . self::PATTERN_HOST);
+        }
+    }
+
+    /**
+     * Change la valeur du port du serveur de BDD en respectant les 
+     * valeurs autorisées : entre 0 et 65535
+     */
+
+    public function setPort(int $newPort)
+    {
+        if ($newPort >= 0 && $newPort <= 65535) {
+            $this->port = $newPort;
+        } else {
+            throw new Exception(__CLASS__ . ' - Le port doit être compris entre 0 et 65535.');
+        }
+    }
+
+    public function setDbname(string $newDbname)
+    {
+        if (preg_match(self::PATTERN_OBJECT, $newDbname) === 1) {
+            $this->dbname = $newDbname;
+        } else {
+            throw new Exception(__CLASS__ . ' - Le nom de la BDD ne correspond pas au modèle demandé : ' . self::PATTERN_OBJECT);
+        }
+    }
+
+    public function setUser(string $newUser)
+    {
+        if (preg_match(self::PATTERN_OBJECT, $newUser) === 1) {
+            $this->user = $newUser;
+        } else {
+            throw new Exception(__CLASS__ . ' - Le nom de l\'utilisateur ne correspond pas au modèle demandé : ' . self::PATTERN_OBJECT);
+        }
+    }
+
+    public function setPass(string $newPass)
+    {
+        if (preg_match(self::PATTERN_PASSWORD, $newPass) === 1) {
+            $this->pass = $newPass;
+        } else {
+            throw new Exception(__CLASS__ . ' - Le mot de passe ne correspond pas au modèle demandé : ' . self::PATTERN_PASSWORD);
+        }
+    }
+
+    /**
+     * Méthode qui renvoie le résultat d'une requête SQL de type SELECT
+     * ou SHOW sous la forme d'un tableau associatif
+     * @author Lesly Lodin <lesly.lodin@baobab-ingenierie.fr>
+     * @version $Revision: 1.0$
+     * @access public
+     * @see https://www.php.net
+     * @param string $sql requête SQL paramétrée de type SELECT ou SHOW 
+     * @param array $params tableau de paramètres pour la requête
+     * @return array résultat de la requête
+     */
+    public function getResult(string $sql, array $params = array()): array
+    {
+        try {
+            // Teste si la requête est de type SELECT ou SHOW
+            $words = explode(' ', strtolower($sql));
+            if ($words[0] === 'select' || $words[0] === 'show') {
+                // Prépare et exécute la requête SQL
+                $res = $this->getCnx()->prepare($sql);
+                $res->execute($params);
+
+                // Renvoie le résultat
+                return $res->fetchAll();
+            } else {
+                throw new Exception(__CLASS__ . ' - La requête doit commencer par SELECT ou SHOW.');
+            }
+        } catch (PDOException $err) {
+            throw new PDOException(__CLASS__ . ' - ' . $err->getMessage());
         }
     }
 }
--- cp7/classes/database.test.php
+++ cp7/classes/database.test.php
@@ -1,8 +1,39 @@
 <?php
 include_once 'database.class.php';
+include_once '../includes/constants.inc.php';
 
 echo '<h2>Instanciation</h2>';
-$db = new Database();
+
+// $db = new Database();
+$db = new Database(HOST, PORT, BASE, USER, PASS);
+
 // $db->host = 'mon_serveur'; // accès public
-$db->setHost('mon_serveur');
+$db->setHost('localhost');
+
+// $db->setPort(-457);
+$db->setPort(3306);
+
+// $db->setDbname('my_database_1');
+$db->setDbname('herblay');
+
+// $db->setUser('lesly3');
+$db->setUser('root');
+
+// $db->setPass('P@ssW_0rd&!');
+$db->setPass('root');
+
 var_dump($db);
+
+// Teste le premier mot d'une requête
+$sql1 = 'UPDATE customers SET fname = \'Tintin\' WHERE id_cust = 12';
+$sql2 = 'SELECT * FROM customers';
+$sql = explode(' ', $sql2);
+echo $sql[0];
+
+// $data = $db->getResult('DELETE FROM customers WHERE id_cust=?', array(100));
+
+$data = $db->getResult('SELECT * FROM customers WHERE fname=?', array('lesly'));
+var_dump($data);
+
+$data = $db->getResult('SHOW tables');
+var_dump($data);
