beb86764db6b2a0779ddb035c25f4889794a80b9
lesly-lodin <lesly.lodin@baobab-ingenierie.fr>
2022-03-03 10:30:48 +0100

    CP7 Day 4

--- cp7/list.php
+++ cp7/list.php
@@ -25,12 +25,27 @@
         $primary = '';
     }
 
+    // Teste si page en cours dans l'URL
+    if (isset($_GET['p']) && !empty($_GET['p'])) {
+        $page = (int) $_GET['p'];
+    } else {
+        $page = 0;
+    }
+
+    // Teste si nb lignes par page dans l'URL
+    if (isset($_GET['n']) && !empty($_GET['n'])) {
+        $nb = (int) $_GET['n'];
+    } else {
+        $nb = 10;
+    }
+
     // Si table et pk vides alors on arrête
     if ($table === '' || $primary === '') {
         echo '<p class="alert alert-warning">Aucune table à exploiter : <a href="index.php">Retour à l\'accueil</a></p>';
         exit();
     } else {
         echo '<h1>Données de la table <strong>' . $table . '</strong></h1>';
+
         echo '<nav aria-label="breadcrumb">
         <ol class="breadcrumb">
           <li class="breadcrumb-item"><a href="index.php">Accueil</a></li>
@@ -48,7 +63,7 @@
         $cnx = new Database(HOST, PORT, BASE, USER, PASS);
 
         // 3. Requête SQL pour lister tous les clients
-        $sql = 'SELECT * FROM ' . $table;
+        $sql = 'SELECT * FROM ' . $table . ' LIMIT ' . $page . ',' . $nb;
         $cols = $cnx->getColumnsName($sql);
         $data = $cnx->getResult($sql);
 
@@ -75,6 +90,27 @@
         $html .= '</table>';
 
         // Affiche le tableau HTML 
+        echo $html;
+
+        // 5. Pagination
+        // Nb total de lignes 
+        $sql2 = 'SELECT COUNT(*) AS total FROM ' . $table;
+        $data2 = $cnx->getResult($sql2);
+        $total = (int) $data2[0]['total'];
+        $pages = ceil($total / $nb);
+
+        // Liens vers pages
+        $html = '<nav><ul class="pagination pagination-sm d-flex flex-wrap">';
+        for ($i = 0; $i < $pages; $i++) {
+            $link = $_SERVER['PHP_SELF'] . '?t=' . $table . '&k=' . $primary . '&p=' . ($i * $nb) . '&n=' . $nb;
+            // Page active ou non ?
+            if ($page === $i * $nb) {
+                $html .= '<li class="page-item active"><span class="page-link">' . ($i + 1) . '</span></li>';
+            } else {
+                $html .= '<li class="page-item"><a class="page-link" href="' . $link . '">' . ($i + 1) . '</a></li>';
+            }
+        }
+        $html .= '</ul></nav>';
         echo $html;
     } catch (Exception $err) {
         echo '<p class="alert alert-danger">' . $err->getMessage() . '<br><a href="index.php">Retour à l\'accueil</a></p>';
